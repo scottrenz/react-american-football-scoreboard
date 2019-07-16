3860897cc7e73d92e66f299e7821548337360263
Scott Renz <scottrenz@scottrenz.com>
2019-07-15 21:19:19 -0400

    made it to add 7 or 3 to scores

--- src/App.js
+++ src/App.js
@@ -28,12 +28,13 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
+          <button onClick={ ()=> setLionScore(7 + LionScore) } className="homeButtons__touchdown">Home Touchdown</button>
+
+          <button  onClick={ ()=> setLionScore(3 + LionScore) }className="homeButtons__fieldGoal">Home Field Goal</button>
+          </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={ ()=> setTigerScore(7 + TigerScore) } className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={ ()=> setTigerScore(3 + TigerScore) } className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
