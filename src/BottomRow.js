37daf2f6da63f05d382b0c3f3c60e9736eca8792
Scott Renz <scottrenz@scottrenz.com>
2019-07-15 21:59:02 -0400

    made click handlers on down and quarter

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, setQuarter] = useState(1); // Give these better names, and decide whether you want to pass an initial score into the state hook as the initialValu
+  const [down, setDown] = useState(1); // Give these better names, and decide whether you want to pass an initial score into the state hook as the initialValu
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div onClick={ ()=> setDown( 1 + down % 4) } className="down__value">{down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,7 +21,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div onClick={ ()=> setQuarter( 1 + quarter % 4) } className="quarter__value">{quarter}</div>
       </div>
     </div>
   );
